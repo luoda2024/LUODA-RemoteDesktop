f9eb97f0574b3f6056ca90fc42b597f3a736a445
luoda2024 <luoda2024@github.com>
2026-05-16 19:55:42 +0800

    fix: APK签名+前景图标+Web渲染+EXE安装程序

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2406,7 +2406,7 @@ class _AboutState extends State<_About> {
                     style: linkStyle,
                   ).marginSymmetric(vertical: 4.0)),
               Container(
-                decoration: const BoxDecoration(color: Color(0xFF2c8cff)),
+                decoration: const BoxDecoration(color: Color(0xFF2A84BA)),
                 padding:
                     const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
                 child: SelectionArea(
