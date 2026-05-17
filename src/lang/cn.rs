d6dc6e23fa8a3cf7e40026cbb6bec578c42169c5
luoda2024 <luoda2024@github.com>
2026-05-17 11:59:35 +0800

    fix: 顶部标题'你的桌面'→'你的ID和密码'，'一次性密码'→'密码'，ID标签保持'ID'

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -2,7 +2,7 @@ lazy_static::lazy_static! {
 pub static ref T: std::collections::HashMap<&'static str, &'static str> =
     [
         ("Status", "状态"),
-        ("Your Desktop", "你的桌面"),
+        ("Your Desktop", "你的ID和密码"),
         ("desk_tip", "你的桌面可以通过下面的 ID 和密码访问。"),
         ("Password", "密码"),
         ("Ready", "就绪"),
@@ -515,7 +515,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Already exists", "已经存在"),
         ("Change Password", "更改密码"),
         ("Refresh Password", "刷新密码"),
-        ("ID", "你的ID"),
+        ("ID", "ID"),
         ("Grid View", "网格视图"),
         ("List View", "列表视图"),
         ("Select", "选择"),
